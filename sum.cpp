e19f0e96c8d9ca10325521bf99ff594218c2cce5
Abhishek Nigam <abhisheknigam17062001@gmail.com>
2021-06-15 13:25:28 +0530

    added function sum

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+int sum(int a,int b){
+    return a+b;
+}
 int main(){
     int a=9;
     int b=8;
 
-    cout<<a+b;
+    cout<<sum(a,b);
     return 0;
 }
